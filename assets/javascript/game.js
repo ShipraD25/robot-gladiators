8adb0e676451757a08ae45cbba3b59d27392a005
Shipra <drshipradyundi@gmail.com>
2020-08-16 18:01:07 -0700

    updated the health function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,22 @@
-// window.alert("This is an alert! JavaScript is running!");
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight () {
-    window.alert("The fight has begun!");
-}
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+  enemyHealth = enemyHealth - playerAttack;
+  // Log a resulting message to the console so we know that it worked.
+   console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+  playerHealth = enemyAttack - playerHealth;
+  // Log a resulting message to the console so we know that it worked.
+console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+};
+
 fight();
